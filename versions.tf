e85fceccbda8f4c427f30caee1ff78226634203b
Mayank Sharma <83959396+mayank0202@users.noreply.github.com>
2023-01-01 05:26:52 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+  }
+}
